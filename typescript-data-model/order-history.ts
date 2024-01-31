7dfdbd564a177c8bd1b85c066be25d27b24fb0f8
Jeffrey Mejia Cortez <jeffreym951@gmail.com>
2024-01-31 00:36:27 +0000

    completed typescipt-data-model

---
+++ typescript-data-model/order-history.ts
@@ -0,0 +1,84 @@
+interface Items {
+  itemType: string;
+  itemCost: number;
+  itemColor: string[];
+  bookTitle?: string;
+  bookAuthor?: string;
+  itemDescription?: string;
+}
+interface Orders {
+  orderNumber: string;
+  orderDate: string;
+  orderDeliveredDate: string;
+  totalCost: number;
+  itemsOrdered?: Items[];
+}
+
+const orderHistory: Orders[] = [
+  {
+    orderNumber: '11439416898772232',
+    orderDate: 'August 4, 2020',
+    orderDeliveredDate: 'August 8, 2020',
+    totalCost: 34.0,
+    itemsOrdered: [
+      {
+        itemType: 'Book',
+        itemCost: 31.55,
+        itemColor: ['Yellow', 'Black'],
+        bookTitle: 'Javascipt for impatient programmers',
+        bookAuthor: 'Rauschmayer, Dr. Axel',
+      },
+    ],
+  },
+  {
+    orderNumber: '11399842681280257',
+    orderDate: 'July 19, 2020',
+    orderDeliveredDate: 'July 20, 2020',
+    totalCost: 44.53,
+    itemsOrdered: [
+      {
+        itemType: 'Book',
+        itemCost: 41.33,
+        itemColor: ['Cream', 'black', 'red'],
+        bookTitle: 'The Timeless Way of Building',
+        bookAuthor: 'Alexander, Christopher',
+      },
+    ],
+  },
+  {
+    orderNumber: '11428755579059409',
+    orderDate: 'July 4, 2020',
+    orderDeliveredDate: 'July 7, 2020',
+    totalCost: 17.22,
+    itemsOrdered: [
+      {
+        itemType: 'Gaming Accessory',
+        itemCost: 15.98,
+        itemColor: ['black'],
+        itemDescription: 'Gamecube Controller Adapter for Wii U and PC',
+      },
+    ],
+  },
+  {
+    orderNumber: '11328831772648248',
+    orderDate: 'July 3, 2020',
+    orderDeliveredDate: 'July 5, 2020',
+    totalCost: 138.93,
+    itemsOrdered: [
+      {
+        itemType: 'Gaming Accessory',
+        itemCost: 94.95,
+        itemColor: ['black', 'grey', 'yellow', 'green', 'red'],
+        itemDescription:
+          'Gamecube Controller -SuperSmash Bros. Edition (Nintendo Switch)',
+      },
+      {
+        itemType: 'Book',
+        itemCost: 33.99,
+        itemColor: ['blue', 'yellow', 'red', 'white', 'grey'],
+        bookTitle: 'The Art of SQL',
+        bookAuthor: 'Faroult, Stephane',
+      },
+    ],
+  },
+];
