e474cf95dbfc6c429c2cd54499614bf278a15dea
Jeffrey Mejia Cortez <jeffreym951@gmail.com>
2024-02-14 00:19:35 +0000

    completed the typescript-modal exercise

---
+++ typescript-modal/main.ts
@@ -0,0 +1,14 @@
+const $open = document.querySelector('.open-modal');
+const $dismiss = document.querySelector('.dismiss-modal');
+const $dialog = document.querySelector('dialog');
+if (!$open || !$dismiss || !$dialog) {
+  throw new Error('The $open, $dismiss, or $dialog query failed');
+}
+
+$open.addEventListener('click', () => {
+  $dialog.showModal();
+});
+
+$dismiss.addEventListener('click', () => {
+  $dialog.close();
+});
