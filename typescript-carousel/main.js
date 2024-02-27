11728cd03d5782359cce9c8322f1caad8dfab9fc
Jeffrey Mejia Cortez <jeffreym951@gmail.com>
2024-02-27 17:02:29 +0000

    made some changes to html and css

--- typescript-carousel/main.js
+++ typescript-carousel/main.js
@@ -3,7 +3,16 @@ const $previous = document.querySelector('.previous-arrow');
 if (!$previous) throw new Error('$previous query has failed');
 const $next = document.querySelector('.next-arrow');
 if (!$next) throw new Error('$next query has failed');
-const $image = document.querySelector('img');
+const $image = document.querySelectorAll('.pokemon');
 if (!$image) throw new Error('$image query has failed');
 const $progressDots = document.querySelectorAll('.dot');
 const $li = document.querySelectorAll('li');
+let count = 0;
+function carousel() {
+  for (let i = 0; i < $li.length; i++) {
+    if ((i = count)) {
+      count++;
+    }
+  }
+}
+$next.addEventListener('click');
--- typescript-carousel/main.ts
+++ typescript-carousel/main.ts
@@ -2,7 +2,21 @@ const $previous = document.querySelector('.previous-arrow');
 if (!$previous) throw new Error('$previous query has failed');
 const $next = document.querySelector('.next-arrow');
 if (!$next) throw new Error('$next query has failed');
-const $image = document.querySelector('img');
+const $image = document.querySelectorAll('.pokemon');
 if (!$image) throw new Error('$image query has failed');
 // const $progressDots = document.querySelectorAll('.dot');
 // const $li = document.querySelectorAll('li')
+
+// let count = 0
+
+// function carousel():void {
+//  for (let i = 0; i< $li.length; i++) {
+//   if (i = count) {
+//     count++
+
+//   }
+//  }
+
+// }
+
+// $next.addEventListener('click', )
