8bfd3595a34355d632b3e32e7995b88d272d1ba8
Jeffrey Mejia Cortez <jeffreym951@gmail.com>
2024-02-27 17:03:43 +0000

    made some changes to html and css

--- typescript-carousel/main.js
+++ typescript-carousel/main.js
@@ -5,14 +5,14 @@ const $next = document.querySelector('.next-arrow');
 if (!$next) throw new Error('$next query has failed');
 const $image = document.querySelectorAll('.pokemon');
 if (!$image) throw new Error('$image query has failed');
-const $progressDots = document.querySelectorAll('.dot');
-const $li = document.querySelectorAll('li');
-let count = 0;
-function carousel() {
-  for (let i = 0; i < $li.length; i++) {
-    if ((i = count)) {
-      count++;
-    }
-  }
-}
-$next.addEventListener('click');
+// const $progressDots = document.querySelectorAll('.dot');
+// const $li = document.querySelectorAll('li')
+// let count = 0
+// function carousel():void {
+//  for (let i = 0; i< $li.length; i++) {
+//   if (i = count) {
+//     count++
+//   }
+//  }
+// }
+// $next.addEventListener('click', )
