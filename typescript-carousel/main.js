0e929b8c688ebaf4fa87e4fb5e25da5fd4d05268
Jeffrey Mejia Cortez <jeffreym951@gmail.com>
2024-02-27 02:20:58 +0000

    started carousel

---
+++ typescript-carousel/main.js
@@ -0,0 +1,9 @@
+'use strict';
+const $previous = document.querySelector('.previous-arrow');
+if (!$previous) throw new Error('$previous query has failed');
+const $next = document.querySelector('.next-arrow');
+if (!$next) throw new Error('$next query has failed');
+const $image = document.querySelector('img');
+if (!$image) throw new Error('$image query has failed');
+const $progressDots = document.querySelectorAll('.dot');
+const $li = document.querySelectorAll('li');
