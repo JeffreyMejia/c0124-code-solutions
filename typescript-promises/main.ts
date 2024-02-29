87d6a85b2e1b959ffb21f07cf25697881580622d
Jeffrey Mejia Cortez <jeffreym951@gmail.com>
2024-02-29 18:38:03 +0000

    made requested changes

--- typescript-promises/main.ts
+++ typescript-promises/main.ts
@@ -1,4 +1,5 @@
 import { takeAChance } from './take-a-chance.js';
 const promise1 = takeAChance('Jeffrey');
-promise1.then((message) => console.log(message));
-promise1.catch((error) => console.log(error));
+promise1
+  .then((message) => console.log(message))
+  .catch((error) => console.log(error));
