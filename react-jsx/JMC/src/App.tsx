d33ee022eb850b06d0298ebf1296e4bf2757302b
Jeffrey Mejia Cortez <jeffreym951@gmail.com>
2024-03-18 19:08:20 +0000

    completed react-jsx exercise

---
+++ react-jsx/JMC/src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React JSX</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Total BUtton Clicks: {count}
+        </button>
+        <p>
+          an extension to JavaScript that allows to write the code for dynamic
+          DOM creation in a syntax similar to HTML
+        </p>
+        <p>
+          By allowing us to write markup directly into a javascript file in
+          combination with javascript.
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
