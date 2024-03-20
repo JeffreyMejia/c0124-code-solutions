40cbeafc3bae4dfe61921e4f55f27b3dff252c8a
Jeffrey Mejia Cortez <jeffreym951@gmail.com>
2024-03-20 23:29:09 +0000

    completed react-stopwatcg exercise

---
+++ react-stopwatch/stopwatch/src/App.tsx
@@ -0,0 +1,13 @@
+import { Stopwatch } from './stopwatch';
+import './stopwatch.css';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Stopwatch playClass="play active" pauseClass="pause" />
+    </>
+  );
+}
+
+export default App;
