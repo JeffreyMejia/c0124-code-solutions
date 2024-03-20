00fbe2a3cd3cf4a6293edb6620cc0a564b7a2f72
Jeffrey Mejia Cortez <jeffreym951@gmail.com>
2024-03-20 19:35:13 +0000

    completed react-hot-button challenge

---
+++ react-hot-button/hot-button/src/App.tsx
@@ -0,0 +1,9 @@
+import { HotButton } from './hotButton';
+import './App.css';
+import './hotButton.css';
+
+function App() {
+  return <HotButton temp="cold" label="Hot Button" />;
+}
+
+export default App;
