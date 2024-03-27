d9bf7f8112ea221f5c2e907303e71f96a42651aa
Jeffrey Mejia Cortez <jeffreym951@gmail.com>
2024-03-27 16:16:21 +0000

    added useCallBack

--- react-carousel/Carousel/src/Carousel.tsx
+++ react-carousel/Carousel/src/Carousel.tsx
@@ -2,7 +2,7 @@ import { Next } from './Next';
 import { Previous } from './Previous';
 import { Indicators } from './Indicators';
 import { Images } from './Images';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 type Props = {
   images: string[];
@@ -10,6 +10,10 @@ type Props = {
 
 export function Carousel({ images }: Props) {
   const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleNext = useCallback(() => {
+    setActiveIndex((activeIndex + 1) % images.length);
+  }, [activeIndex, images]);
 
   useEffect(() => {
     const intervalId = setTimeout(handleNext, 3000);
@@ -24,10 +28,6 @@ export function Carousel({ images }: Props) {
 
   function handlePrevious() {
     setActiveIndex((activeIndex - 1 + images.length) % images.length);
-  }
-
-  function handleNext() {
-    setActiveIndex((activeIndex + 1) % images.length);
   }
 
   return (
